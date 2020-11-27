73cfa9e211488c146f0a04f0856c5e69cfd28cc6
Justin Aiello <justinaiello@gmail.com>
2020-11-27 13:05:27 -0500

    feat: add model, update rubocop config

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,4 @@
+# Movies Controller - generated with 'rails g controller movies'
 class MoviesController < ApplicationController
   def index
     @movies = ['Iron Man', 'Spiderman', 'Superman', 'Batman']
---
+++ app/models/movie.rb
@@ -0,0 +1,3 @@
+# Movie Model - generated with 'rails g model movie title:string rating:string total_gross:decimal'
+class Movie < ApplicationRecord
+end
