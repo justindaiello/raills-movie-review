3eacabe1eef0f1abb462a6d981f83da9e9e933e2
Justin Aiello <justinaiello@gmail.com>
2020-11-27 13:27:58 -0500

    feat: connect model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 # Movies Controller - generated with 'rails g controller movies'
 class MoviesController < ApplicationController
   def index
-    @movies = ['Iron Man', 'Spiderman', 'Superman', 'Batman']
+    @movies = Movie.all
   end
 end
