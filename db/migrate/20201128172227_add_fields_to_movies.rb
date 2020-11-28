69891d604903e1d7a71b2ba170760f611ac3f96c
Justin Aiello <justinaiello@gmail.com>
2020-11-28 12:29:09 -0500

    feat: add new migration

---
+++ db/migrate/20201128172227_add_fields_to_movies.rb
@@ -0,0 +1,7 @@
+# Adds two new fields to movies table
+class AddFieldsToMovies < ActiveRecord::Migration[6.0]
+  def change
+    add_column :movies, :description, :text
+    add_column :movies, :released_on, :date
+  end
+end
