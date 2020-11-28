bb294c458df5adc7348c62f00a95039d2127e35f
Justin Aiello <justinaiello@gmail.com>
2020-11-28 13:17:07 -0500

    feat: create view and model helpers

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 # Movie Model - generated with 'rails g model movie title:string rating:string total_gross:decimal'
 class Movie < ApplicationRecord
+  def flop?
+    total_gross.blank? || total_gross < 225_000_000
+  end
 end
