da684dea66cd703bdf578c046a0b056a072ef9cf
Justin Aiello <justinaiello@gmail.com>
2021-01-10 15:28:42 -0500

    feat: add basic auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back #{user.name}"
+    else
+      flash.now[:alert] = 'Invalid username/password combination!'
+      render :new
+    end
+  end
 
   def destroy; end
 end
