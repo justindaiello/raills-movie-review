825a3487b336ab3dd73811fc1b86a564de5654e5
Justin Aiello <justinaiello@gmail.com>
2021-04-10 17:29:01 -0400

    feat: add require sign in authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   add_flash_types(:danger)
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def require_signin
+    redirect_to new_session_url, alert: 'You must be signed in!' unless current_user
+  end
 end
